264adeef0f59be7e33b515f55c8850a968e5b94d
Hung <mongtamquoc2015@gmail.com>
2026-05-16 15:16:05 +0700

    fix: Define ransackable_attributes in User model

--- template/profiles/api/app/models/user.rb
+++ template/profiles/api/app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   rolify
 
   validates :email, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "id", "name", "updated_at"]
+  end
 end
--- template/profiles/web_auth/app/models/user.rb
+++ template/profiles/web_auth/app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
   def inactive_message
     discarded? ? :inactive : super
   end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "id", "name", "updated_at"]
+  end
 end
